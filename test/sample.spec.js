f03265879096864a9621bab236ee3d154af5ae0e
Ankit Muchhala <muchhalaankit@gmail.com>
2017-11-21 00:50:36 +0530

    integrate puppeteer into the testing framework

--- test/sample.spec.js
+++ test/sample.spec.js
@@ -1,7 +1,6 @@
-const { expect } = require('chai');
-
 describe('sample test', function () {
-  it('should work', function () {
+  it('should work', async function () {
+    console.log(await browser.version());
     expect(true).to.be.true;
   });
 });
