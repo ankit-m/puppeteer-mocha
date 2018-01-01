e0c7055c1e7588d930f0370dd9cc01bc4b3e3a3e
Ankit Muchhala <muchhalaankit@gmail.com>
2018-01-01 18:52:38 +0530

    add a submit dialog

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
-function submitForm(e, form) {
+function submitForm(e) {
+  const data = 'Email:' + document.getElementById('user-email').value + '\n' +
+    'Select:' + document.getElementById('user-select').value + '\n';
+
+  window.confirm(data);
   e.preventDefault();
 }
